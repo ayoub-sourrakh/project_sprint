1990d1b6473814e5f4a73aedd66946fc8e9db345
ayoub <ayoub.sourrakh@outlook.com>
2024-04-26 01:18:31 +0200

    table product

--- bluelry_b/db/schema.rb
+++ bluelry_b/db/schema.rb
@@ -11,10 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_25_015247) do
-  create_table "products", force: :cascade do |t|
+  create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.decimal "price"
+    t.decimal "price", precision: 10
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
