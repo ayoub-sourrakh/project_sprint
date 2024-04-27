5d19f84d816b9a21444084564576167dc24cd9ff
ayoub-sourrakh <ayoub.sourrakh@outlook.com>
2024-04-27 20:43:54 +0000

    production.rb

--- bluelry_b/config/environments/production.rb
+++ bluelry_b/config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts = []
   config.hosts << "bluelry.com"
   config.hosts << "www.bluelry.com"
 end
