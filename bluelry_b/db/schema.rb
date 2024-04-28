f0598f9086f6fb90bc32b4a501a8579c0f58f3ac
ayoub <ayoub.sourrakh@outlook.com>
2024-04-28 23:54:11 +0200

    user controller

--- bluelry_b/db/schema.rb
+++ bluelry_b/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_015247) do
-  create_table "products", force: :cascade do |t|
+ActiveRecord::Schema[7.1].define(version: 2024_04_28_035326) do
+  create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.decimal "price"
+    t.decimal "price", precision: 10
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
